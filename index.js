cbbbaa0b0304ae48649b8de28557e5cb6474215e
mercyrazak8867-ui <mercyrazak8867@gmail.com>
2026-09-23 17:08:21 +0530

    Update index.js
    razak

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static('public'));
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/pair', async (req, res) => {
-  let number = req.query.number;
-  if (!number) return res.json({ error: 'Number daalo vai' });
-  
-  number = number.replace(/[^0-9]/g, '');
-  
-  try {
-    // Baileys ka code yaha ayega, pehle site to chalne de
+Baileys ka code yaha ayega, pehle site to chalne de
     const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
     const { state, saveCreds } = await useMultiFileAuthState('/tmp/session');
     
